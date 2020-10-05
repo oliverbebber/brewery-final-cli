b270228e6952da7d1258f6c1a5fba6523e20331b
Oliver Bebber <ogbebber@gmail.com>
2020-10-05 15:42:42 -0400

    brewery methods

--- lib/brewery.rb
+++ lib/brewery.rb
@@ -1,3 +1,21 @@
 class Brewery
-
+  attr_accessor :name, :brewery_id, :state, :city
+ 
+  @@all = []
+  
+  def initialize(name:, brewery_id:, state:)
+    @name = name
+    @brewery_id = brewery_id
+    @state = state
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.find_by_state(state)
+    @@all.select { |br| br.state == state }
+  end
+ 
 end
