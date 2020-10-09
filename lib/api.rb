bbe1c536b5f33f8c7ab3fc17c127394eeaa88e3e
Oliver Bebber <ogbebber@gmail.com>
2020-10-09 01:35:43 -0400

    give user more options and fixed infinite loop

--- lib/api.rb
+++ lib/api.rb
@@ -7,9 +7,9 @@ class API
     response = Net::HTTP.get(uri)
     breweries = JSON.parse(response)
     breweries.each do |br|
+      # binding.pry
       Brewery.new(name: br["name"], brewery_id: br["id"], state: state)
     end
-    binding.pry
   end
 
   def self.get_brewery_details(brewery)
@@ -17,12 +17,12 @@ class API
     uri = URI(url)
     response = Net::HTTP.get(uri)
     brew = JSON.parse(response)
+    # binding.pry
     brewery.brewery_type = brew["brewery_type"]
     brewery.city = brew["city"]
     brewery.street = brew["street"]
     brewery.postal_code = brew["postal_code"]
     brewery.phone = brew["phone"]
     brewery.website_url = brew["website_url"]
-    binding.pry
   end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -29,24 +29,56 @@ class CLI
     API.fetch_breweries(@state)
     puts ""
     breweries = Brewery.all
-    print_brewery(breweries)
+    print_breweries(breweries)
     puts ""
-    puts "Type in a number to see more details, type 'exit' to leave directory."
+    puts "Type in a number to see more details"
+    puts "OR type 'state' to search breweries in a different state"
+    puts "OR type 'list' to see the brewery list again"
+    puts "OR type 'exit' to exit."
     puts ""
     input = gets.chomp.downcase # make sure to add something to allow user to exit - typing in exit states "...a list found in EXIT"
     while input != 'exit' do
-      brewery = Brewery.find_by_state(@state)[input.to_i - 1]
-      API.get_brewery_details(brewery)
+      if input == 'list'
+        print_breweries(Brewery.find_by_state(@state))
+      elsif input.to_i > 0 && input.to_i <= Brewery.find_by_state(@state).length
+        brewery = Brewery.find_by_state(@state)[input.to_i - 1]
+        API.get_brewery_details(brewery)
+        print_brewery(brewery)
+      else
+        puts "invalid input"
+      end
+      input = gets.chomp.downcase
     end
-    # binding.pry
+    puts ""
+    puts "Cheers!" # come back and use beer emoji/ascii for farewell
   end
   
-  def print_brewery(brew)
+  def print_breweries(br)
     puts ""
     puts "Here is a list of the breweries we found in #{@state.split.map(&:capitalize).join(' ')}." # capitalizes 1st letter of each word for @state; ex: new york = New York
     puts ""
-    brew.each_with_index do |br, i|
-      puts "#{i+1}. #{br.name}"
+    br.each_with_index do |b, i|
+      puts "#{i+1}. #{b.name}"
     end
+  end
+
+  def print_brewery(brewery)
+    puts ""
+    puts "#{brewery.name} details"
+    puts "--------------------------------------------------"
+    puts "This is a #{brewery.brewery_type} brewery."
+    puts "--------------------------------------------------"
+    puts "Address:"
+    puts ""
+    puts "#{brewery.street}" 
+    puts "#{brewery.city}, #{brewery.state.split.map(&:capitalize).join(' ')}"
+    puts "#{brewery.postal_code}"
+    puts "--------------------------------------------------"
+    puts "Contact #{brewery.name}"
+    "--------------------------------------------------"
+    puts "By phone: #{brewery.phone}" # find method to split hash to make phone number xxx-xxx-xxxx
+    puts ""
+    puts "Webpage: #{brewery.website_url}"
+    puts ""
   end
 end
